097d4969e356f79d0a5e222652bd4eceab3974b5
Andrey Ignatov <hiller994@gmail.com>
2025-03-04 23:44:05 +0300

    Исправления по ДЗ 8: - Убрать () в def check_quantity - В ошибку добавлено какой именно товар отсутствует - Исправлена функция удаления товаров из корзины - Убрал переменную QUANTITY_OF_REQUESTED_PRODUCTS из файла models.py - добавил тесты на добавление и удаление разного кол-ва товаров из корзины

--- homework/models.py
+++ homework/models.py
@@ -1,9 +1,6 @@
-QUANTITY_OF_REQUESTED_PRODUCTS = 800
-
 class Product:
-    """
-    Класс продукта (есть некий продукт)
-    """
+
+    #Класс продукта (есть некий продукт)
     name: str #имя
     price: float #цена
     description: str #описание
@@ -16,34 +13,20 @@ class Product:
         self.quantity = quantity
 
     def check_quantity(self, quantity) -> bool:
-        return (quantity <= self.quantity)
-        """
-        TODO Верните True если количество продукта больше или равно запрашиваемому
-            и False в обратном случае
-        """
+        return quantity <= self.quantity
 
     def buy(self, quantity):
         if self.check_quantity(quantity): #если выполняются условия функции
             self.quantity -= quantity # то количество товара на складе уменьшается на количество купленного товара
             return self.quantity
         else:
-            raise ValueError (f"Недостаточно товара на складе. Запрошено: {quantity}, доступно: {self.quantity}")
+            raise ValueError (f"Недостаточно товара: {self.name} на складе. Запрошено: {quantity}, доступно: {self.quantity}")
 
-        """
-        TODO реализуйте метод покупки
-            Проверьте количество продукта используя метод check_quantity
-            Если продуктов не хватает, то выбросите исключение ValueError
-        """
 
     def __hash__(self):
         return hash(self.name + self.description)
 
-class Cart:
-    """
-    Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
-    TODO реализуйте все методы класса
-    """
-
+class Cart: # Класс корзины
     # Словарь продуктов и их количество в корзине
     products: dict[Product, int]
 
@@ -51,25 +34,15 @@ class Cart:
         # По-умолчанию корзина пустая
         self.products = {}
 
-    def add_product(self, product: Product, buy_count=1) ->None:
+    def add_product(self, product: Product, buy_count=1) ->None: # Метод добавления продукта в корзину
         if self.products.get(product):  # если продукт есть в корзине
             self.products[product] += buy_count #то мы добавляем товар
         else:
             self.products[product] = buy_count #добавляем новую запись в словать
 
-        """
-        Метод добавления продукта в корзину.
-        Если продукт уже есть в корзине, то увеличиваем количество
-        """
-
     def remove_product(self, product: Product, remove_count=None):
-        """
-        Метод удаления продукта из корзины.
-        Если remove_count не передан, то удаляется вся позиция
-        Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
-        """
-        if remove_count is None:
-            del self.products[product]
+        if remove_count is None or remove_count >= self.products[product]:  # Если remove_count больше или равен количеству продукта в корзине,
+            del self.products[product]  # всё удаляем
         else:
             self.products[product] -= remove_count
 
@@ -85,14 +58,9 @@ class Cart:
 
 
     def buy(self): #можем купить все товары в корзине
-        """
-        Метод покупки.
-        Учтите, что товаров может не хватать на складе.
-        В этом случае нужно выбросить исключение ValueError
-        """
         for product, quantity in self.products.items():
             if product.quantity < quantity:
-                raise ValueError("Такого кол-во товара нет в наличии")
+                raise ValueError(f"Недостаточно товара: {product.name} Запрошено: {quantity}, доступно: {product.quantity}")
 
         for product, quantity in self.products.items():
             product.buy(quantity)
--- homework/test_shop.py
+++ homework/test_shop.py
@@ -1,10 +1,7 @@
-"""
-Протестируйте классы из модуля homework/models.py
-"""
-
+QUANTITY_OF_REQUESTED_PRODUCTS = 800
 import pytest
 
-from homework.models import Product, QUANTITY_OF_REQUESTED_PRODUCTS, Cart
+from homework.models import Product, Cart
 
 
 @pytest.fixture
@@ -21,11 +18,6 @@ def cart():
 
 
 class TestProducts:
-    """
-    Тестовый класс - это способ группировки ваших тестов по какой-то тематике
-    Например, текущий класс группирует тесты на класс Product
-    """
-
     def test_product_check_quantity(self, product): #проверяем кол-во
         assert product.check_quantity(QUANTITY_OF_REQUESTED_PRODUCTS)
         # TODO напишите проверки на метод check_quantity
@@ -34,24 +26,13 @@ class TestProducts:
         in_stock = product.quantity
         product.buy(QUANTITY_OF_REQUESTED_PRODUCTS)
         assert product.quantity == in_stock - QUANTITY_OF_REQUESTED_PRODUCTS
-        # TODO напишите проверки на метод buy
 
     def test_product_buy_more_than_available (self, product): #пытаемся купить больше, чем доступно
         with pytest.raises(ValueError):
             product.buy(3000)
-        # TODO напишите проверки на метод buy,
-        #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
 
 
-class TestCart: #тест корзины
-
-    """
-    TODO Напишите тесты на методы класса Cart
-        На каждый метод у вас должен получиться отдельный тест
-        На некоторые методы у вас может быть несколько тестов.
-        Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
-    """
-
+class TestCart: # тест корзины
     def test_add_product(self, product, cart):
         count_of_first = 10
         cart.add_product(product=product, buy_count=count_of_first) #к словарю применяем функцию
@@ -61,10 +42,32 @@ class TestCart: #тест корзины
         assert cart.products[product] == count_of_first + count_of_second #получается, что в данным момент не создаем, а добавляем к текущему значению
 
     def test_remove_product(self, cart, product): # удаление продукта из корзины
-        add_count = 10
+        # удалили частично (добавили 3, удалили 2)
+        add_count = 3
         cart.add_product(product, buy_count=add_count)
         cart.remove_product(product, remove_count=2)
-        assert cart.products[product] == 8
+        assert cart.products[product] == 1
+
+    def test_remove_product2(self, cart, product):
+        # удалили частично (добавили 3, удалили 3)
+        add_count = 3
+        cart.add_product(product, buy_count=add_count)
+        cart.remove_product(product, remove_count=3)
+        assert product not in cart.products
+
+    def test_remove_product3(self, cart, product):
+        # удалили без количества (добавили 3, удалили)
+        add_count = 3
+        cart.add_product(product, buy_count=add_count)
+        cart.clear()
+        assert product not in cart.products
+
+    def test_remove_product4(self, cart, product):
+        # удалили больше чем было (добавили 3, пробуем удалить 4)
+        add_count = 3
+        cart.add_product(product, buy_count=add_count)
+        cart.remove_product(product, remove_count=4)
+        assert product not in cart.products
 
     def test_clear(self, cart, product): # очистить всю корзину
         cart.clear()
@@ -85,3 +88,13 @@ class TestCart: #тест корзины
         cart.buy()
         assert product.quantity == 990 #остатки стока1
         assert product2.quantity == 80 #остатки стока2
+
+    # случай, когда добавили пару разных продуктов, но не хватает одного из них
+    def test_buy2(self, cart, product,  product2):
+        quantity1 = 10  # Количество первого продукта
+        quantity2 = 101  # Количество второго продукта (но на складе 100)
+        cart.add_product(product, buy_count=quantity1)
+        cart.add_product(product2, buy_count=quantity2)
+        with pytest.raises(ValueError) as exc_info:
+            cart.buy()
+        assert str(exc_info.value) == f"Недостаточно товара: {product2.name} Запрошено: {quantity2}, доступно: {product2.quantity}"
